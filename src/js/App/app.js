3a63afbba30f458902165e0dee7ef14a95bce29b
Sander Boelhouwers <sboelhouwers@upcmail.nl>
2019-03-14 23:33:03 +0100

    Added basic router and theme option in css.

--- src/js/App/app.js
+++ src/js/App/app.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {BrowserRouter} from 'react-router-dom';
 import Page from '../Components/Page';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
@@ -6,11 +7,13 @@ import Footer from '../Components/Footer';
 export default class App extends Component {
     render() {
         return (
-            <div>
-                <Header/>
-                <Page/>
-                <Footer/>
-            </div>
+            <BrowserRouter>
+                <div>
+                    <Header/>
+                    <Page/>
+                    <Footer/>
+                </div>
+            </BrowserRouter>
         );
     }
 }
